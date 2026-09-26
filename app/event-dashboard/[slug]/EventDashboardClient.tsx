7478e006c61936fc7d4beac20a8a9266342abed1
Maleesha Amendra <72781485+Maleesh3038@users.noreply.github.com>
2026-09-26 21:13:16 +0530

    Update EventDashboardClient.tsx

--- app/event-dashboard/[slug]/EventDashboardClient.tsx
+++ app/event-dashboard/[slug]/EventDashboardClient.tsx
@@ -1094,7 +1094,6 @@ export default function EventDashboardClient({ slug }: { slug: string }) {
                         )}
                       </div>
                       )}
-                      </div>
                     </div>
                   ))}
                 </div>
